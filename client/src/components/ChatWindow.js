abf21637963178c9a3901e394c6c91c2903b046f
Dirk Dobson <dirkvancouver@live.com>
2018-09-27 10:01:54 -0600

    window

---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { setFlash } from '../reducers/flash'
+import { addMessage } from '../reducers/messages'
+import { Segment, Header, Form, TextArea, Button} from 'semantic-ui-react'
+export default
