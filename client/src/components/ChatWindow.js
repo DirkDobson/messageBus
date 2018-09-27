6eda5a4bae6bbab833c55f2a86aa846a2dc3a615
Dirk Dobson <dirkvancouver@live.com>
2018-09-27 10:56:20 -0600

    protectedRoute

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -2,41 +2,54 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { setFlash } from '../reducers/flash'
 import { addMessage } from '../reducers/messages'
-import { Segment, Header, Form, TextArea, Button} from 'semantic-ui-react'
-//import ChatMessage from './ChatMessage'
+import { 
+  Segment,
+  Header,
+  Form,
+  TextArea,
+  Button,
+} from 'semantic-ui-react'
 import styled from 'styled-components'
+import ChatMessage from './ChatMessage'
 
 const MainWindow = styled(Segment)`
   border: 3px solid black;
   height: 60vh;
-  overflow=y: scrool;
+  overflow-y: scroll;
   background-color: lightgray;
   border-radius: 10px;
-  `
+`
 
 const MessageInput = styled(Segment)`
-border-radius: 10px;
-width: 80%;
-margin: 0 auto;
-padding 10px;
+  border-radius: 10px;
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px;
 `
-
-
 
 const Underline = styled(Header)`
-text-decoration: underline;
+  text-decoration: underline;
 `
 
-
 class ChatWindow extends React.Component {
-  state = {message: ''}
+  state = { message: '' }
 
-  conponentDidMount() {
-    this.props.dispatch(setFlash('Welcom to React Chat', 'blue'))
+  componentDidMount() {
+    this.props.dispatch(setFlash('Welcome to React Chat', 'green'))
   }
-  displayMessages = () =>{
+  
+  displayMessages = () => {
+    const { messages } = this.props
 
-  } 
+    if (messages.length) 
+      return messages.map( (m,i) => <ChatMessage key={i} {...m} /> )
+
+    return (
+      <Segment inverted textAlign="center">
+        <Header as="h1">No Messages Yet</Header>
+      </Segment>
+    )
+  }
 
   setMessage = (e) => {
     this.setState({ message: e.target.value })
@@ -44,35 +57,36 @@ class ChatWindow extends React.Component {
 
   addMessage = (e) => {
     e.preventDefault()
-    const { dispatch, user: {email} } =this.props
-    const {message} = this.state
-    dispatch(addMessage({ email, body: message}))
-    this.setState({ message: ''})
+    const { dispatch, user: { email } } = this.props
+    const { message } = this.state
+    dispatch(addMessage({ email, body: message }))
+    this.setState({ message: '' })
   }
-    render(){
-    return(
+
+  render() {
+    return (
       <Segment basic>
-      <Underline as="h2" textAlign="center">
-      React Chat!
-      </Underline>
-      <MainWindow basic>
-        {this.displayMessages() }
-      </MainWindow>
-      <MessageInput>
-        <Form onSubmit={this.addMessage}>
-        <TextArea
-        value={this.state.message}
-        onChange={this.setMessage}
-        placeholder="Say Something"
-        auotFocus
-        required
-        >
-        </TextArea>
-        <Segment basic textAlign="center">
-        <Button primary> Send Message</Button>
-        </Segment>
-        </Form>
-      </MessageInput>
+        <Underline as="h2" textAlign="center">
+          React Chat!
+        </Underline>
+        <MainWindow basic>
+          { this.displayMessages() }
+        </MainWindow>
+        <MessageInput>
+          <Form onSubmit={this.addMessage}>
+            <TextArea
+              value={this.state.message}
+              onChange={this.setMessage}
+              placeholder="Say something nice..."
+              autoFocus
+              required
+            >
+            </TextArea>
+            <Segment basic textAlign="center">
+              <Button primary>Send Message</Button>
+            </Segment>
+          </Form>
+        </MessageInput>
       </Segment>
     )
   }
@@ -85,4 +99,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect (mapStateToProps) (ChatWindow)
+export default connect(mapStateToProps)(ChatWindow)
